3dd17cdab25a54a8f19f14c3c118e091bfb65f78
zAnz <baidosovich@gmail.com>
2024-05-24 17:33:06 +0500

    login page view

--- client/app/auth/login/page.tsx
+++ client/app/auth/login/page.tsx
@@ -1,7 +1,47 @@
-export default function Page()  {
+import React from "react";
+import Image from "next/image";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
     return (
-        <div>
-            <h1>фыв</h1>
+        <div className={'flex flex-col p-10'}>
+            <div className={'col-span-2 mb-5'}>
+                <div className={'flex justify-between items-center'}>
+                    <h1 className={'text-3xl font-bold'}>Task Manager</h1>
+                    <div>Manage your tasks efficiently</div>
+                </div>
+            </div>
+            <div className={'flex flex-row justify-center'}>
+                <div className={'flex flex-col items-center bg-content px-20 py-14 gap-3 rounded-l-2xl'}>
+                    <h2 className={'text-2xl font-bold'}>Organize your tasks with ease.</h2>
+                    <h3 className={'text-xl'}>Navigate seamlessly with the docked bar.</h3>
+                    <Image
+                        src={'/img.png'}
+                        alt={'sad'}
+                        width={256}
+                        height={256}
+                    />
+                    <p className={'text-sm text-gray-400 mt-4'}>Access your tasks from anywhere</p>
+                </div>
+                <div className={'flex flex-col items-center px-20 py-14 gap-3'}>
+                    <h2 className={'text-2xl font-bold'}>Get started with TaskDock.</h2>
+                    <p className={'text-lg text-gray-400 '}>Explore the features free.</p>
+                    <Input placeholder={'Enter your full name'}/>
+                    <Input placeholder={'Create a unique username'}/>
+                    <Input placeholder={'Set your password'}/>
+                    <Input placeholder={'Confirm your password'}/>
+
+                    <Button variant="outline" className={'w-full'}> Sign up</Button>
+
+                    <div>or sign in with</div>
+
+                    <div className={'flex gap-2'}>
+                        <Button variant="outline" className={'w-1/2'}>Google</Button>
+                        <Button variant="outline" className={'w-1/2'}>GitHub</Button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,27 +1,33 @@
-import { Inter } from "next/font/google";
+import {Inter} from "next/font/google";
 import "./globals.css";
+import './app.css'
 import React from "react";
 import {cn} from "@/lib/utils";
 import {Metadata} from "next";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+    subsets: ["latin"],
+    weight: ["400", "500", "700"],
+});
 
-export const metadata : Metadata = {
-  title: "MyTasks",
-  authors: {
-    name: "zAnz",
-    url: "https://github.com/zanz9",
-  }
+export const metadata: Metadata = {
+    title: "MyTasks",
+    authors: {
+        name: "zAnz",
+        url: "https://github.com/zanz9",
+    }
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ru">
-      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.className) }>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="ru">
+        <body className={cn("min-h-screen _bg-background antialiased", inter.className)}>
+        {children}
+        </body>
+        </html>
+    );
 }
